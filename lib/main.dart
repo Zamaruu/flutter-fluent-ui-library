32d9bb44765ce2a9f85641887dd988a07b1d7956
Maxmilian Ditz <ditzma@gmx.de>
2021-03-22 17:36:40 +0100

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui_design/routes/mainpage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(FluentUIDesign());
@@ -9,18 +10,13 @@ class FluentUIDesign extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    debugPaintSizeEnabled = false;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+        ),
         primarySwatch: Colors.blue,
       ),
       home: MainPage(),
--- lib/routes/mainpage.dart
+++ lib/routes/mainpage.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
+  TextStyle style = new TextStyle(
+    color: Colors.black
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        leadingWidth: 150,
+        leading: Container(
+          margin: EdgeInsets.only(left: 10),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/microsoft_logo.png")
+            )
+          ),
+        ),
+        title: Text(
+          "Fluent UI Design Flutter",
+          style: style,
+        ),
+      ),
     );
   }
 }
